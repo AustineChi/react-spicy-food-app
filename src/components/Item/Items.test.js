c9a95c7559953daebcb3fb265f02fa59a6c775c0
AustineChi <emeghara.augustine@gmail.com>
2021-03-06 23:58:31 +0100

    removed space

--- src/components/Item/Items.test.js
+++ src/components/Item/Items.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, cleanup } from "@testing-library/react";
+import { render, cleanup} from "@testing-library/react";
 import "@testing-library/jest-dom";
 import Item from "./Item";
 import meals from "../../data.json";
